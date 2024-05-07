8a343df6ffd418d46319539fe9ff1117a241be9a
giang.vo@spclt.com.vn <giang.vo@spclt.com.vn>
2024-05-07 22:00:56 +0700

    update bai 3

--- CShapeK4/Program.cs
+++ CShapeK4/Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
-
+using Library;
 namespace CShapeK4
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello K4");
-            Console.ReadLine();
-            Console.WriteLine("Xin chao các bạn");
-        }
-    }
+    //bắt đầu
+    //class Program
+    //{
+    //    static void PrintDay()
+    //    {
+    //        Console.WriteLine(DateTime.Now);
+    //    }
+    //    static void Main(string[] args)
+    //    {
+    //        NhanVien.PhongBan("Hello K4");
+
+    //        Console.WriteLine("Hello K4");
+    //        Console.ReadLine();
+    //        Console.WriteLine("Xin chao các bạn");
+    //    }
+    //}
+//kết th
 }
---
+++ Library/NhanVien.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class NhanVien
+    {
+        public static string Name = "Nguyen Van A";
+        public static void PhongBan(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
